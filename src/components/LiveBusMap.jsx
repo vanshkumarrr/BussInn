576116e82ff4fdd13c1ba6bcf404d43cc50839e5
Keshav Kumar <maybecontentcreater@gmail.com>
2026-08-12 02:54:38 +0530

    feat: improve dynamic live bus tracking

--- src/components/LiveBusMap.jsx
+++ src/components/LiveBusMap.jsx
@@ -1,10 +1,30 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useEffect } from "react";
+
+// Automatically moves the map when the bus location changes
+function MapUpdater({ latitude, longitude }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (latitude && longitude) {
+      map.setView([latitude, longitude]);
+    }
+  }, [latitude, longitude, map]);
+
+  return null;
+}
 
 function LiveBusMap({
   latitude = 28.6139,
   longitude = 77.2090,
-  busNumber = "Bus #27",
+  busNumber = "Bus #27"
 }) {
   return (
     <MapContainer
@@ -17,11 +37,20 @@ function LiveBusMap({
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
+      <MapUpdater
+        latitude={latitude}
+        longitude={longitude}
+      />
+
       <Marker position={[latitude, longitude]}>
         <Popup>
-          🚌 {busNumber}
+          <strong>🚌 {busNumber}</strong>
           <br />
           Live Location
+          <br />
+          Latitude: {latitude}
+          <br />
+          Longitude: {longitude}
         </Popup>
       </Marker>
     </MapContainer>
